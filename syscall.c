d643846bf189449e794aac13bd40927f99e813b6
Julian Engel <me@julianengel.com>
2016-09-26 14:14:00 -0400

    Added kernel space declaration; added pstree to syscall dispatch table

--- syscall.c
+++ syscall.c
@@ -77,6 +77,7 @@ argstr(int n, char **pp)
   return fetchstr(addr, pp);
 }
 
+extern int sys_pstree(void);
 extern int sys_chdir(void);
 extern int sys_close(void);
 extern int sys_dup(void);
@@ -121,6 +122,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_pstree]  sys_pstree,
 };
 
 void
