7af3fd2681c82bb225c3911ae54c99f4ab018870
Julian Engel <me@julianengel.com>
2016-09-29 10:02:14 -0400

    Implemented proper use of malloc

--- pstree.c
+++ pstree.c
@@ -2,11 +2,16 @@
 #include "stat.h"
 #include "user.h"
 
+// Maximum number of processes to record
+#define MAX_PROCS 64
+
 int
 main(int argc, char *argv[])
 {
 
-  //TODO: make array of uproc structs
+  
+  //Make array of uproc structs dynamically
+  struct uproc* procs = malloc((sizeof(struct uproc) * MAX_PROCS));
 
   exit();
 }
